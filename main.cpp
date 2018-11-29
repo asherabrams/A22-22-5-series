364020ae82d214427d26e55fbd6be81051961fee
Asher Abrams <asher813@aol.com>
2018-11-29 05:43:23 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  A22-2112-series
+//
+//  Created by Asher Abrams on 2018/1/22.
+//  Copyright © 2018 Asher Abrams. All rights reserved.
+//  2018K29 05:35.  This code generates a list of Fibonacci numbers.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+
+long int a=0;
+long int b=1;
+long int c=0;
+long int i=1;
+
+int main(int argc, const char * argv[]) {
+    // Fibonacci numbers
+    
+    for (i=1; i <= 40; i++)
+    {
+        a = b;
+        b = c;
+        c = a + b;
+        cout << c <<'\n';
+    }
+    
+    return 0;
+}
+
